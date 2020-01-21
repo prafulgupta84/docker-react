d9e63711a5f5ef2d8d8c159dd75b284c91e82d62
Ubuntu <ubuntu@ip-172-31-81-189.ec2.internal>
2020-01-21 11:09:49 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Changed after docker compose addition
         </p>
         <a
           className="App-link"
